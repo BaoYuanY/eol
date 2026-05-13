6c283111e8e3f11990f96836eae88f1b99b998b9
BaoYuan <itbaoyuan@163.com>
2026-05-14 03:51:46 +0800

    添加浏览器显示logo

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,6 +8,7 @@
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
+        <link rel="icon" href="https://itbaoyuan.cn/storage/logo.svg">
         <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
 
         <!-- Styles / Scripts -->
