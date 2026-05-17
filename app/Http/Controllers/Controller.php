1f75bed4a0d85a45060224f24b7ec43086dc1c32
BaoYuan <itbaoyuan@163.com>
2026-05-18 01:43:25 +0800

    添加 api 路由文件  php artisan install:api

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,7 +2,23 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Http\JsonResponse;
+
 abstract class Controller
 {
-    //
+
+    public function json(array $data = []): JsonResponse
+    {
+        return response()->json($data)->header('Content-Type', 'application/json;charset=utf-8');
+    }
+
+    public function success(array $data = null): JsonResponse
+    {
+        return $this->json(['code' => 200, 'msg' => '', 'data' => $data ?: null]);
+    }
+
+    public function error(int $code, string $msg = '', array $data = null): JsonResponse
+    {
+        return $this->json(['code' => $code, 'msg' => $msg, 'data' => $data ?: null]);
+    }
 }
